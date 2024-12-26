5381f1da7ea94022a7bda9a9dfde279c2f64d048
Hassan Raza <razah1532@gmail.com>
2024-12-26 11:46:39 +0500

    Pointers

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void swap(int* num1, int* num2)
+{
+    int temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
+}
+int main() {
+    int a, b;
+    cout<<"Enter two numbers:"<<endl;
+    cin>>a>>b;
+
+    cout<<"Before Swapping:"<<endl;
+    cout<<"Num 1: "<< a<< " Num 2: "<<b<<endl;
+
+    swap(&a, &b);
+    cout<<"After Swapping:"<<endl;
+    cout<<"Num 1: "<< a<< " Num 2: "<<b;
+}
